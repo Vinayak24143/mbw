60ba2d0560a7facc652ed28f161da81746679485
Vinayak24143 <vinayak24143@gmail.com>
2022-09-25 16:02:54 +0530

    feat(images added to bazaar app models)

--- bazaar/models.py
+++ bazaar/models.py
@@ -1,23 +1,68 @@
 from django.db import models
 
+def bazaarImage(instance, filename):
+    return '/'.join( ['bazaar', str(instance.id), filename] )
+
+
+def productImage(instance, filename):
+    return '/'.join( ['product', str(instance.id), filename] )
+
+def bazaarGroupImage(instance, filename):
+    return '/'.join( ['bazaargroup', str(instance.id), filename] )
+
+def productCategoryImage(instance, filename):
+    return '/'.join( ['prductcategory', str(instance.id), filename] )
+
+def productSubCategoryImage(instance, filename):
+    return '/'.join( ['prductsubcategory', str(instance.id), filename] )
+
+
 class Group(models.Model):
     name = models.CharField(max_length=255)
-
+    image = models.ImageField(
+        upload_to=bazaarGroupImage,
+        max_length=254, blank=True, null=True
+    )
     def __str__(self) -> str:
         return self.name
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
+    brand = models.CharField(max_length=255, null=True, blank=True)
+    gst = models.IntegerField(null=True)
+    hsnCode = models.CharField(max_length=100, null=True)
+
+    frontImage = models.ImageField(
+        upload_to=productImage,
+        max_length=254, blank=True, null=True
+    )
+    backImage = models.ImageField(
+        upload_to=productImage,
+        max_length=254, blank=True, null=True
+    )
+    mrpImage = models.ImageField(
+        upload_to=productImage,
+        max_length=254, blank=True, null=True
+    )
+
     def __str__(self) -> str:
         return self.name
 
 class ProductCategory(models.Model):
     name = models.CharField(max_length=255)
+    image = models.ImageField(
+        upload_to=productCategoryImage,
+        max_length=254, blank=True, null=True
+    )
     def __str__(self) -> str:
         return self.name
 
 class ProductSubCategory(models.Model):
     name = models.CharField(max_length=255)
+    image = models.ImageField(
+        upload_to=productSubCategoryImage,
+        max_length=254, blank=True, null=True
+    )
 
     def __str__(self) -> str:
         return self.name
@@ -31,6 +76,11 @@ class Bazaar(models.Model):
     productCategory = models.ManyToManyField(ProductCategory)
     productSubCategory = models.ManyToManyField(ProductSubCategory)
     product = models.ManyToManyField(Product)
+    image = models.ImageField(
+        upload_to=bazaarImage,
+        max_length=254, blank=True, null=True
+    )
 
     def __str__(self) -> str:
         return self.name
+    
--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os 
 
 import django_heroku
 import environ
@@ -124,6 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL='/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
