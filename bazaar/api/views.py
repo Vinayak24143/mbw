4588a7ef754f058dc1e8a65422298febd6defe73
Vinayak24143 <vinayak24143@gmail.com>
2022-09-28 07:40:09 +0530

    feat(filter for bazaar based on cities)

--- bazaar/api/views.py
+++ bazaar/api/views.py
@@ -1,10 +1,29 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, exceptions
 from bazaar.models import *
 from .serializers import *
+import json
+
 
 class BazaarViewSet(viewsets.ModelViewSet):
     queryset = Bazaar.objects.all()
     serializer_class = BazaarSerializer
+
+    def get_queryset(self):
+        queryset = Bazaar.objects.all()
+        cities = self.request.query_params.get('cities')
+        
+        if cities is not None:
+            try:
+                cities = json.loads(cities)
+                if type(cities) is not list:
+                    raise exceptions.ValidationError("cities query perameter must be a list of integers")
+            except json.decoder.JSONDecodeError:
+                raise exceptions.ValidationError("cities query perameter must be a list of integers")
+            except:
+                pass
+
+            queryset = queryset.filter(city__id__in=cities)
+        return queryset
 
 class BazaarGroupCategoryViewSet(viewsets.ModelViewSet):
     queryset = GroupCategory.objects.all()
